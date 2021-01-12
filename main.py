88e2eba7f387e44cf6784d274c68480d4ada1ef8
Rishav Chanda <rishavchanda0@gmail.com>
2021-01-12 13:56:56 +0530

    faceRecognition

--- main.py
+++ main.py
@@ -1,23 +1,58 @@
 import cv2
 import numpy as np
 import face_recognition
+import os
 
-imgRishav = face_recognition.load_image_file('images/rishav2.jpg')
-imgRishav=cv2.cvtColor(imgRishav,cv2.COLOR_BGR2RGB)
+path = 'images'
+images=[]
 
-imgRishavTest = face_recognition.load_image_file('images/rishav2.jpg')
-imgRishavTest=cv2.cvtColor(imgRishavTest,cv2.COLOR_BGR2RGB)
+classNames = []
+myList = os.listdir(path)
+print(myList)
 
-faceloc = face_recognition.face_locations(imgRishav)[0]
-encodeElon = face_recognition.face_encodings(imgRishav)[0]
-cv2.rectangle(imgRishav,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(0,255,0),2)
+for cl in myList:
+    currentImg = cv2.imread(f'{path}/{cl}')
+    images.append(currentImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+print(classNames)
+
+def findEncoadings(images):
+    encodingList = []
+    for img in images:
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodingList.append(encode)
+    return encodingList
+
+encodeListKnown = findEncoadings(images)
+print('Encoading Done')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success,img = cap.read()
+    imgs= cv2.resize(img,(0,0),None,0.25,0.25)
+    imgs = cv2.cvtColor(imgs,cv2.COLOR_BGR2RGB)
 
 
-facelocTest = face_recognition.face_locations(imgRishavTest)[0]
-encodeElonTest = face_recognition.face_encodings(imgRishavTest)[0]
-cv2.rectangle(imgRishavTest,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(0,255,0),2)
+    facesCurrFrame = face_recognition.face_locations(imgs)
+    encodesCurrFrame = face_recognition.face_encodings(imgs,facesCurrFrame)
 
+    for encodeFace,faceLoc in zip(encodesCurrFrame,facesCurrFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
 
-cv2.imshow('Rishav Chanda',imgRishav)
-cv2.imshow('Rishav Test',imgRishavTest)
-cv2.waitKey(0)
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
